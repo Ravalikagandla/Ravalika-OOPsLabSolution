f2f72c23a472638780efde3d9664dc52ea780137
Ravalikagandla <107806397+Ravalikagandla@users.noreply.github.com>
2022-06-20 18:27:35 +0530

    Add files via upload

---
+++ service/CredentialService.java
@@ -0,0 +1,40 @@
+package com.greatlearning.service;
+	
+	import java.util.Random;
+
+	import com.greatlearning.model.Employee;
+
+	public class CredentialService {
+		public char[] generatePassword() {
+
+			String CapitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			String SmallLetters = "abcdefghijklmnopqrstuvwxyz";
+			String Numbers = "0123456789";
+			String SpecialCharacters = "!@#$%^&*+_?',<>";
+			String values = CapitalLetters + SmallLetters + Numbers + SpecialCharacters;
+
+			Random random = new Random();
+			char[] password = new char[8];
+			for (int i = 0; i < 8; i++) {
+				password[i] = values.charAt(random.nextInt(values.length()));
+			}
+			return password;
+		}
+
+		public String generateEmailAddress(String firstName, String lastName, String Department) {
+			return firstName + lastName + "@" + Department + ".abc.com";
+
+		}
+
+		public void showCredentials(Employee employee, String email, char[] generatedPassword) {
+			System.out.println("Dear " + employee.getFirstName() + " your generated credentials as follows");
+			System.out.println("Email  --->" + email);
+			System.out.print("Password ---> + 1 " + "");
+			System.out.print(generatedPassword);
+
+
+
+		}
+	} 
+
+
